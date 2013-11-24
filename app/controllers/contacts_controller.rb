75a41e8884d77ad5aa18b32102be973fa6459516
noahpatterson <noah@noah-patterson.com>
2013-11-24 10:03:40 -0500

    add jquery ajax form submit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -12,18 +12,26 @@ class ContactsController < ApplicationController
 
 	def create
 		@contact = Contact.new(secure_params)
-		if @contact.valid?
-			#todo save data
-			@contact.update_spreadsheet
-			#todo send message
-			# UserMailer.contact_email(@contact).deliver
-			MailWorker.perform_async(@contact.as_json)
-			flash[:notice] = "Message sent from #{@contact.name}"
-			# usflash[:notice] = "Message sent to Google Drive"
-			redirect_to root_path
-		else
-			render :new
+		respond_to do |format|
+			if @contact.valid?
+				#todo save data
+				@contact.update_spreadsheet
+				#todo send message
+				# UserMailer.contact_email(@contact).deliver
+				MailWorker.perform_async(@contact.as_json)
+				# flash[:notice] = "Message sent from #{@contact.name}"
+				# flash[:notice] = "Message sent to Google Drive"
+				# redirect_to root_path
+				
+				format.html {redirect_to root_path, flash[:notice] = "Message sent from #{@contact.name}"}
+				format.js 
+				
+			else
+				format.html {redirect_to root_path}
+				format.js {render 'error'}
+			end
 		end
+
 	end
 
 	private
